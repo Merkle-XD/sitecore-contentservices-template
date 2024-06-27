5813938da79bff27068d7e44aac84fbeffbdcbf7
cheeto-bandito <chet.potvin@merkle.com>
2024-06-27 11:37:55 -0500

    Updated to reflect latest changes from xmcloud-foundation-head

--- README.md
+++ README.md
@@ -2,31 +2,19 @@
 
 ## QUICK START
 
-1. Setup your environment variables
-
-    ```
-    Copy-Item '.env-template' '.env'
-    ```
-2. In an ADMIN terminal:
+1. In an ADMIN terminal:
 
     ```ps1
     .\init.ps1 -InitEnv
     ```
 
-3. Restart your terminal and run:
+2. Restart your terminal and run:
 
     ```ps1
     .\up.ps1
     ```
 
-3. Follow the instructions to [deploy to XM Cloud](#deploy-to-xmcloud)
-
-4. Create Edge token and [query from edge](#query-edge)
-
-*** 
-
 ## About this Solution
+
 This solution is designed to help developers learn and get started quickly
 with XMCLoud + SXA.
-
-
--- init.ps1
+++ init.ps1
@@ -4,10 +4,21 @@ Param (
         ParameterSetName = "env-init")]
     [switch]$InitEnv,
 
-    [Parameter(
+    [Parameter(Mandatory = $true,
         HelpMessage = "The path to a valid Sitecore license.xml file.",
         ParameterSetName = "env-init")]
-    [string]$LicenseXmlPath = "c:\sitecore\license\license.xml"
+    [string]$LicenseXmlPath = "c:\sitecore\license\license.xml",
+
+    # We do not need to use [SecureString] here since the value will be stored unencrypted in .env,
+    # and used only for transient local development environments.
+    [Parameter(Mandatory = $true,
+        HelpMessage = "Sets the sitecore\\admin password for this environment via environment variable.",
+        ParameterSetName = "env-init")]
+    [string]$AdminPassword,
+
+    [Parameter(Mandatory = $false, HelpMessage = "Specifies os version of the base image.")]
+    [ValidateSet("ltsc2019", "ltsc2022")]
+    [string]$baseOs = "ltsc2019"
 )
 
 $ErrorActionPreference = "Stop";
@@ -22,6 +33,8 @@ if ($InitEnv) {
     }
     # We actually want the folder that it's in for mounting
     $LicenseXmlPath = (Get-Item $LicenseXmlPath).Directory.FullName
+
+    Copy-Item .env-template .env -Force
 }
 
 Write-Host "Preparing your Sitecore Containers environment!" -ForegroundColor Green
@@ -32,11 +45,11 @@ Write-Host "Preparing your Sitecore Containers environment!" -ForegroundColor Gr
 
 # Check for Sitecore Gallery
 Import-Module PowerShellGet
-$SitecoreGallery = Get-PSRepository | Where-Object { $_.SourceLocation -eq "https://sitecore.myget.org/F/sc-powershell/api/v2" }
+$SitecoreGallery = Get-PSRepository | Where-Object { $_.SourceLocation -eq "https://nuget.sitecore.com/resources/v2" }
 if (-not $SitecoreGallery) {
     Write-Host "Adding Sitecore PowerShell Gallery..." -ForegroundColor Green
     Unregister-PSRepository -Name SitecoreGallery -ErrorAction SilentlyContinue
-    Register-PSRepository -Name SitecoreGallery -SourceLocation https://sitecore.myget.org/F/sc-powershell/api/v2 -InstallationPolicy Trusted
+    Register-PSRepository -Name SitecoreGallery -SourceLocation https://nuget.sitecore.com/resources/v2 -InstallationPolicy Trusted
     $SitecoreGallery = Get-PSRepository -Name SitecoreGallery
 }
 
@@ -137,17 +150,17 @@ if ($InitEnv) {
     Set-EnvFileVariable "RENDERING_HOST" -Value "www.$Subdomain.localhost"
 
     # REPORTING_API_KEY = random 64-128 chars
-    Set-EnvFileVariable "REPORTING_API_KEY" -Value (Get-SitecoreRandomString 128 -DisallowSpecial)
+    Set-EnvFileVariable "REPORTING_API_KEY" -Value "'$(Get-SitecoreRandomString 128 -DisallowSpecial)'"
 
     # TELERIK_ENCRYPTION_KEY = random 64-128 chars
-    Set-EnvFileVariable "TELERIK_ENCRYPTION_KEY" -Value (Get-SitecoreRandomString 128 -DisallowSpecial)
+    Set-EnvFileVariable "TELERIK_ENCRYPTION_KEY" -Value "'$(Get-SitecoreRandomString 128)'"
 
     # MEDIA_REQUEST_PROTECTION_SHARED_SECRET
-    Set-EnvFileVariable "MEDIA_REQUEST_PROTECTION_SHARED_SECRET" -Value (Get-SitecoreRandomString 64 -DisallowSpecial)
+    Set-EnvFileVariable "MEDIA_REQUEST_PROTECTION_SHARED_SECRET" -Value "'$(Get-SitecoreRandomString 64)'"
 
     # SQL_SA_PASSWORD
     # Need to ensure it meets SQL complexity requirements
-    Set-EnvFileVariable "SQL_SA_PASSWORD" -Value (Get-SitecoreRandomString 19 -DisallowSpecial -EnforceComplexity)
+    Set-EnvFileVariable "SQL_SA_PASSWORD" -Value "'$(Get-SitecoreRandomString 19 -DisallowSpecial -EnforceComplexity)'"
 
     # SQL_SERVER
     Set-EnvFileVariable "SQL_SERVER" -Value "mssql"
@@ -155,7 +168,14 @@ if ($InitEnv) {
     # SQL_SA_LOGIN
     Set-EnvFileVariable "SQL_SA_LOGIN" -Value "sa"
 
-    Set-EnvFileVariable "SITECORE_ADMIN_PASSWORD" -Value "'$(Get-SitecoreRandomString 19 -DisallowSpecial -EnforceComplexity)'"
+    # SITECORE_ADMIN_PASSWORD
+    Set-EnvFileVariable "SITECORE_ADMIN_PASSWORD" -Value $AdminPassword
+
+    # SITECORE_VERSION
+    Set-EnvFileVariable "SITECORE_VERSION" -Value "1-$baseOS"
+
+    # EXTERNAL_IMAGE_TAG_SUFFIX
+    Set-EnvFileVariable "EXTERNAL_IMAGE_TAG_SUFFIX" -Value $baseOS
 }
 
 Write-Host "Done!" -ForegroundColor Green
